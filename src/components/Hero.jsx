3160effb62a8972a2bba164473e2f5e82953d67d
SahilGunjal <sahilsgunjal@gmail.com>
2023-08-15 17:28:18 -0500

    comment ComputerCanvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto mb-[10px] xs:mb-[100px]">
+    <section className="relative w-full h-screen mx-auto xs:mb-[100px] mb-[10px] ">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className="flex flex-col justify-center items-center mt-5">
@@ -25,7 +25,7 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
       <div className="absolute xs:bottom-0 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[45px] rounded-2xl border-4 border-secondary flex justify-center items-start p-2">
