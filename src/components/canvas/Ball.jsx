debf827f31e40fba2dac6e37b3dc27f30940e8f4
SahilGunjal <sahilsgunjal@gmail.com>
2023-08-15 18:16:11 -0500

    uncomment

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,26 +14,26 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <>
+    <Float>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#fff8eb"
-          // polygonOffset
-          // polygonOffsetFactor={-5}
-          // flatShading
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
         />
         <Decal
           position={[0, 0, 1]}
-          // rotation={[2 * Math.PI, 0, 6.25]}
+          rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
           map={decal}
-          // flatShading
+          flatShading
         />
       </mesh>
-    </>
+    </Float>
   );
 };
 
