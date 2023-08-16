32dcc1c96b201154cfa753d9df971bd429795198
SahilGunjal <sahilsgunjal@gmail.com>
2023-08-15 21:36:26 -0500

    Final

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -5,10 +5,10 @@ import { motion } from "framer-motion";
 
 function Technology() {
   return (
-    <div className="flex flex-wrap gap-10 justify-center p-10 mt-5 mb-5">
+    <div className="flex flex-wrap gap-8 justify-center p-10 mt-5 mb-5">
       {technologies.map((technology) => (
         <motion.div
-          className="bg-gradient-to-r from-green-300 to-blue-400 w-28 h-28 rounded-full text-[50px] text-white-100
+          className="bg-gradient-to-r from-green-300 to-blue-400 w-20 h-20 md:h-28 md:w-28 rounded-full text-[50px] text-white-100
           flex justify-center items-center hover:from-blue-500 hover:to-green-500"
           key={technology.name}
           whileHover={{ scale: 1.1 }}
@@ -17,7 +17,7 @@ function Technology() {
           <motion.img
             src={technology.icon}
             alt={technology.name}
-            className="h-20 w-20"
+            className="h-12 w-12 md:h-20 md:w-20"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
           />
