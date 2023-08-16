a446c43dbcfebd157ee2a7ea7bb2dea74e8fefec
SahilGunjal <sahilsgunjal@gmail.com>
2023-08-15 21:31:24 -0500

    final_final

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 function Technology() {
   return (
-    <div className="flex flex-wrap gap-16 justify-center p-10 mt-5 mb-5">
+    <div className="flex flex-wrap gap-10 justify-center p-10 mt-5 mb-5">
       {technologies.map((technology) => (
         <motion.div
           className="bg-gradient-to-r from-green-300 to-blue-400 w-28 h-28 rounded-full text-[50px] text-white-100
