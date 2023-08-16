908760c7d469681ee5b25af5ccaef746a1a57fa4
SahilGunjal <sahilsgunjal@gmail.com>
2023-08-15 19:50:14 -0500

    add rotate

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -57,7 +57,7 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
-          enableRotate={false}
+          // enableRotate={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
